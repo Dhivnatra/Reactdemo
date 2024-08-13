6caa9aa2a2e8293b55da2a8d1605ef5a79c2875b
Dhivnatra <dhivyanatarajan@gmail.com>
2024-08-13 13:00:47 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 
 #!/bin/bash
 echo "$GIT_BRANCH"
-if [[ "$GIT_BRANCH" == "origin/dev" ]]; then
+if [ "$GIT_BRANCH" == "origin/dev" ]; then
     ./build.sh
     docker tag reactimg "$DOCKER_REACT_IMAGED1"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker push "$DOCKER_REACT_IMAGED1"
 
-elif [[ "$GIT_BRANCH" == "origin/main" ]]; then
+elif [ "$GIT_BRANCH" == "origin/main" ]; then
     ./build.sh
     docker tag reactimg "$DOCKER_REACT_IMAGEP1"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
